d90f1da407254497dfc13cb4ec18f029acab2a3a
Ricardo Ruiz Anaya <ricardo._.ra@hotmail.com>
2016-12-09 08:17:28 +0100

    Ruta instanciada

---
+++ Espacio De Aventuras/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene: MonoBehaviour {
+
+	public void loadAScene (string nameScene) {
+		SceneManager.LoadScene (nameScene);
+	}
+}
