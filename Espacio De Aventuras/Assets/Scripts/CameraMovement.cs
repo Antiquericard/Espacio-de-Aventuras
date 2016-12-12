9e2e18a0c980a17e219ebf5227ec49e713f16374
Nicolás <nirobayo@gmail.com>
2016-12-12 19:30:47 +0100

    Primera versión de scripts. Cámara no funcionando pero bases establecidas.

---
+++ Espacio De Aventuras/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	[SerializeField]
+	float scaleRotationX = 0.2f;
+	[SerializeField]
+	float scaleRotationY = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (scaleRotationY * Input.GetAxis("Mouse Y"),scaleRotationX * Input.GetAxis("Mouse X"), 0f);
+	}
+}
---
+++ Espacio De Aventuras/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	string [] levelTexts;
+	int level = 1;
+	float sqrMinVelocity;
+	BoxCollider bounds;
+
+	//0: Modo de apuntar cámara 1: Modo de potencia 2: Modo disparado
+	public static short mode = 0;
+
+	[SerializeField]
+	float startingVelocity = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
