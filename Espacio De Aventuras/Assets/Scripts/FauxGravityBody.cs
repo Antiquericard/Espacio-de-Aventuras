cbbda549c8b8891016fba9cd693cf2dc836b0685
Ricardo Ruiz Anaya <ricardo._.ra@hotmail.com>
2016-12-13 18:48:46 +0100

    no message

---
+++ Espacio De Aventuras/Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+
+	private Transform myTransform;
+
+	void Start () {
+		myTransform = transform;
+	}
+
+	void Update () {
+		//Función de atracción del objeto de este script hacia otros objetos.
+		attractor.Attract (myTransform);
+	}
+}
