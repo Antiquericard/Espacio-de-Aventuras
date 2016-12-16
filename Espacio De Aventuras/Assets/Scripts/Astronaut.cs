19926d9f863853ff99a05d80b52f309c3a8a7835
Nicolás <nirobayo@gmail.com>
2016-12-16 14:39:11 +0100

    Añadido el astronauta

---
+++ Espacio De Aventuras/Assets/Scripts/Astronaut.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Astronaut : MonoBehaviour {
+
+	public void Init(float speed, Transform cannon){
+		transform.parent = cannon;
+		transform.localPosition = new Vector3 (0f, 0f, 1.5f);
+		transform.localEulerAngles = Vector2.zero;
+		transform.parent = null;
+		//GetComponent<Rigidbody> ().velocity = cannon.GetComponent<Rigidbody> ().velocity;
+		GetComponent<Rigidbody> ().AddForce (transform.forward * speed, ForceMode.Impulse);
+		//GetComponent<Rigidbody> ().velocity = speed * transform.forward;
+	}
+
+}
