e7f8083bf589341c215ce7e4e3780d0878aaa84d
Ricardo Ruiz Anaya <ricardo._.ra@hotmail.com>
2016-12-16 14:08:46 +0100

    Interfaz versión 1 finalizada, al darle al botón de inicio cargaría una lista de botones que será la selección de niveles, una vez seleccionado uno se cargará el nivel.

---
+++ Espacio De Aventuras/Assets/Scripts/SeleccionadorNiveles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SeleccionadorNiveles : MonoBehaviour {
+
+	[Tooltip("Colocar el botón a repetir según el número de niveles.")] [SerializeField] GameObject boton;
+
+	[Tooltip("Colocar el padre de la posicion en la jerarquía del botón.")] [SerializeField] Transform parent;
+
+	Vector3 posicion = new Vector3 (0f, 130f, 0f);
+
+	void Start () {
+		GameObject but = (GameObject) Instantiate(boton, parent);
+		but.transform.localPosition = posicion;
+	}
+}
