520805997ea86c9a31eebd3a9f114f36351a8ab8
Nicolás <nirobayo@gmail.com>
2017-01-12 11:38:32 +0100

    - Añadida clase externa para guardar y cargar partida, de utilidad

---
+++ Espacio De Aventuras/Assets/Scripts/SaveGameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveGameManager {
+    
+	public static void Save (int level) {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/levelSave.dat",FileMode.OpenOrCreate);
+        bf.Serialize(file, level);
+        file.Close();
+	}
+	
+
+	public static int Load () {
+		if(File.Exists(Application.persistentDataPath + "/levelSave.dat")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/levelSave.dat", FileMode.Open);
+            int level = (int)bf.Deserialize(file);
+            file.Close();
+            return level;
+        }
+        return 1;
+
+	}
+}
