787349b34fe04f64e1a382d0d46bac6523a7b192
Nicolás <nirobayo@gmail.com>
2017-01-24 12:02:09 +0100

    fail

---
+++ Espacio De Aventuras/Assets/ShipCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (GameManager._instance.mode == GameManager.ShootingMode.Returning && other.transform.CompareTag("PlayerLose")) {
+			other.transform.GetComponent<Astronaut> ().returned = true;
+		}
+	}
+}
