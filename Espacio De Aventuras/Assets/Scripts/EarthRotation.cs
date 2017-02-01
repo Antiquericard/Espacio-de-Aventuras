bb86b7f146b10ec58fa9a90c0257a46b36fdc288
Ricardo Ruiz Anaya <ricardo._.ra@hotmail.com>
2017-02-01 11:55:00 +0100

    Añadida tierra.

---
+++ Espacio De Aventuras/Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotation : MonoBehaviour {
+
+	[Tooltip("Velocidad a la que rota la Tierra.")] [SerializeField] float rotateVelocity = 5f;
+
+	void LateUpdate () {
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(.1f,1f,.1f) * rotateVelocity * Time.deltaTime);
+	}
+}
