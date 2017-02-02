3967406560c8e621507a4143cc7a6c78aa8c219f
Ricardo Ruiz Anaya <ricardo._.ra@hotmail.com>
2017-02-02 13:51:38 +0100

    Nave instanciada, pero hace falta corregir algunos detalles.

---
+++ Espacio De Aventuras/Assets/Scripts/LookAt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	[Tooltip("Cámara a la cual tiene que mirar este GameObject.")] [SerializeField] GameObject cam;
+
+	void lateUpdate () {
+		transform.LookAt (cam.transform);
+	}
+}
